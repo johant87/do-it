258f51f719cb151bfde2ce53a8bdc2e75b9fa515
Artur Petrosyan <a.petrosyan@live.nl>
2016-02-25 14:26:14 +0100

    bootstrap added

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './stylesheets/components.scss';
+import React from 'react';
+import jQuery from 'jquery';
+import ProjectList from './ProjectList';
+
+class App extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Pick your Project</h1>
+        < ProjectList />
+      </div>
+      );
+  }
+}
+
+export default App;
